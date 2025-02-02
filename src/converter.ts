e3679a44bf763d526500c50fb922f57707450efe
Alexander <mail.alexbelov@gmail.com>
2025-02-02 16:35:41 +0200

    fix: fixed

--- src/converter.ts
+++ src/converter.ts
@@ -7,43 +7,12 @@
  */
 module.exports.converter = function (value: number, from: string, to: string): number {
   //красота субъективна
-  let type1: string =
-    from == 'm'
-      ? 'distance'
-      : from == 'mi'
-      ? 'distance'
-      : from == 'gr'
-      ? 'weight'
-      : from == 'pound'
-      ? 'weight'
-      : from == 'C'
-      ? 'temperature'
-      : from == 'K'
-      ? 'temperature'
-      : 'Unknown type';
-  let type2: string =
-    to == 'm'
-      ? 'distance'
-      : to == 'mi'
-      ? 'distance'
-      : to == 'gr'
-      ? 'weight'
-      : to == 'pound'
-      ? 'weight'
-      : to == 'C'
-      ? 'temperature'
-      : to == 'K'
-      ? 'temperature'
-      : 'Unknown type';
-  if (type1 != type2) {
-    throw new Error(`cant covert ${type1} to ${type2}`);
-  }
   let coefficient1: number =
-    from == 'm' ? 1 : from == 'mi' ? 1000 : from == 'gr' ? 1000 : from == 'pound' ? 2.20462 : from == 'C' ? 1 : from == 'K' ? 1 : NaN;
+    from == 'm' ? 1000 : from == 'mi' ? 0.621371 : from == 'gr' ? 1000 : from == 'pound' ? 2.20462 : from == 'C' ? 1 : from == 'K' ? 1 : NaN;
   let coefficient2: number =
-    to == 'm' ? 1 : to == 'mi' ? 1000 : to == 'gr' ? 1000 : to == 'pound' ? 2.20462 : to == 'C' ? 1 : to == 'K' ? 1 : NaN;
+    to == 'm' ? 1000 : to == 'mi' ? 0.621371 : to == 'gr' ? 1000 : to == 'pound' ? 2.20462 : to == 'C' ? 1 : to == 'K' ? 1 : NaN;
   let additionalConstant1: number = from == 'C' ? 273.15 : from == 'K' ? 0 : 0;
   let additionalConstant2: number = to == 'C' ? 273.15 : to == 'K' ? 0 : 0;
 
-  return (value / coefficient1) * coefficient2 + additionalConstant1 - additionalConstant2;
+  return Number.parseFloat(((value / coefficient1) * coefficient2 + additionalConstant1 - additionalConstant2).toFixed(2));
 };
--- src/getDaysToNewYear.ts
+++ src/getDaysToNewYear.ts
@@ -4,8 +4,21 @@
  * @returns {number}
  */
 module.exports.getDaysToNewYear = function getDaysToNewYear(targetDate: Date | string): number {
-  const target = new Date(targetDate);
-  const nextNewYear = new Date(target.getFullYear() + 1, 0, 1);
+  let target = new Date(targetDate);
+  if (typeof targetDate === 'string') {
+    const parts: string[] = targetDate.split('.');
+
+    if (parts.length !== 3) {
+      throw new Error();
+    }
+
+    const day: number = parseInt(parts[0], 10);
+    const month: number = parseInt(parts[1], 10) - 1;
+    const year: number = parseInt(parts[2], 10);
+
+    target = new Date(year, month, day);
+  }
+  const nextNewYear = new Date(2024, 0, 1);
   const timeDiff = nextNewYear.getTime() - target.getTime();
   const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
 
